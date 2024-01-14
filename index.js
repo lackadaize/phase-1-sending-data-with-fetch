875f582f6621783fd4856ae922e16044ca464b84
Jonathan Kulak <lackadaize@msn.com>
2024-01-14 15:51:29 -0500

    This is the first commit for the Sending Data with Fetch assignment.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData () {
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+};
+
+return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (object) {
+         document.querySelector('body').append(object.id);
+    })
+    .catch(function (error) {
+          document.querySelector('body').append(error.message);
+    });
+};
